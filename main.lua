7bab5ba63d1bcd3aa29d765562f9134d34e1cafe
Josh <39926382+Hirora@users.noreply.github.com>
2019-12-02 00:39:54 +0000

    Major Overhaul

--- main.lua
+++ main.lua
@@ -14,14 +14,15 @@ end
 local function usage ()
   io.stderr:write("Usage:\n")
   io.stderr:write(
+  	  "\n",
       " < -r > < -id > < game id >\n"
     )
   io.stderr:write("\n")
   io.stderr:write("Example:\n")
   io.stderr:write(
+  	  "\n",
       " Run CS:GO : srunner -r -id 730\n",
-      "\n",
-      " Yes, I do play CS:GO a lot\n"
+      "\n"
     )
   io.stderr:write("\n")
   io.stderr:flush()
@@ -34,21 +35,183 @@ local function Set (list)
   return set
 end
 
+function tablelength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
+end
+
 local expectedArgs = {
-	["ARG1"] = Set {
-		"-r"
+	["EXCEPTIONS"] = {
+		["-r"] = {
+			["ARG2"] = Set {
+				"APPID"
+			},
+			["ARG3"] = Set {
+				"APPID",
+				"NIL"
+			}
+		},
+		["-ls"] = {
+			["ARG2"] = Set {
+				"NIL"
+			},
+			["ARG3"] = Set {
+				"NIL"
+			}
+		}
 	},
 
-	["ARG2"] = Set {
-		"-id"
+	["-r"] = {
+		["ARG2"] = Set {
+			"-id"
+		},
+		["ARG3"] = Set {
+
+		}
+	},
+
+	["-ls"] = {
+		["ARG2"] = Set {
+			
+		},
+		["ARG3"] = Set {
+
+		}
 	}
 }
 
-local ARG1		= select(1, ...) or usage()
-local ARG2		= select(2, ...) or usage()
-local ARG3		= select(3, ...) or usage()
+local argFunctions = {
+	["-r"] = function ( argument2 , argument3 ) 
+		if argument2 == "-id" then
+			io.stderr:write('App loading...' , "\n")
+			os.execute("steam steam://rungameid/" .. argument3 .. "&> /dev/null 2>&1")
+		else
+			io.stderr:write('App loading...' , "\n")
+			os.execute("steam steam://rungameid/" .. argument2 .. "&> /dev/null 2>&1")
+		end
+	end,
+	
+	["-ls"] = function ()
+		io.write('Apps:\n')
 
-local function checkArgs ( argument1 , argument2 )
+		local jsonLib = io.open("library.json","r+")
+		local jsonContent = jsonLib:read()
+		jsonLib:close()
+		local library = json.decode(jsonContent)
+
+		local count = 1
+
+		for i,v in pairs(library.Apps) do
+			if count < tablelength(library.Apps) then
+				io.write('├┬ ',i,"\n")
+				io.write('┆└─╼ App ID: ' , v.ID , '\n')
+				io.write('┆\n')
+				
+			else
+				io.write('└┬ ',i,"\n")
+				io.write(' └─╼ App ID: ' , v.ID , '\n')
+			end
+
+			count = count+1
+		end
+
+	end
+}
+
+local ARG1		= select(1, ...)
+local ARG2		= select(2, ...)
+local ARG3		= select(3, ...)
+
+function isID ( suspect )
+	local valid = false
+
+	local jsonLib = io.open("library.json","r+")
+	local jsonContent = jsonLib:read()
+	jsonLib:close()
+	local library = json.decode(jsonContent)
+
+	for i,v in pairs(library.Apps) do
+		if library.Apps[i]["ID"] == suspect then
+			valid = true
+		end
+	end
+
+	return valid
+end
+
+function expectsID ( argTable )
+	local valid = false
+
+	for i,v in pairs(argTable) do
+		if i == "APPID" then
+			valid = true
+		end
+	end
+
+	return valid
+end
+
+function expectsNIL ( argTable )
+	local valid = false
+
+	for i,v in pairs(argTable) do
+		if i == "NIL" then
+			valid = true
+		end
+	end
+
+	return valid
+end
+
+function validateArgs ( argument1 , argument2 , argument3 )
+	local valid = false
+
+	local arg1Valid = false
+	local arg2Valid = false
+	local arg3Valid = false
+
+	if expectedArgs[argument1] then
+		arg1Valid = true
+	else
+		io.stderr:write("Argument type: '" .. argument1 .. "' is not valid or does not exist; run 'srunner --help'" , "\n")
+	end
+
+	if arg1Valid then
+		if expectedArgs[argument1].ARG2[argument2] then
+			arg2Valid = true
+		elseif isID(argument2) and expectsID(expectedArgs.EXCEPTIONS[argument1].ARG2) or expectsNIL(expectedArgs.EXCEPTIONS[argument1].ARG3) then
+			arg2Valid = true
+		else
+			if argument2 then
+				io.stderr:write("Argument type: '" .. argument2 .. "' is not valid or does not exist; run 'srunner --help'" , "\n")
+			else
+				io.stderr:write("Argument 2 is not valid or does not exist; run 'srunner --help'" , "\n")
+			end
+			
+		end
+
+		if expectedArgs[argument1].ARG3[argument3] then
+			arg3Valid = true
+		elseif isID(argument3) and expectsID(expectedArgs.EXCEPTIONS[argument1].ARG3) or expectsNIL(expectedArgs.EXCEPTIONS[argument1].ARG3) then
+			arg3Valid = true
+		else
+			if argument3 then
+				io.stderr:write("Argument type: '" .. argument3 .. "' is not valid or does not exist; run 'srunner --help'" , "\n")
+			else
+				io.stderr:write("Argument 3 is not valid or does not exist; run 'srunner --help'" , "\n")
+			end
+		end
+
+		if arg1Valid and arg2Valid and arg3Valid then
+			valid = true
+		end
+	end
+
+	return valid
+end
+
+--[[local function checkArgs ( argument1 , argument2 )
 	local arg1Valid = false
 	local arg2Valid = false
 
@@ -61,13 +224,27 @@ local function checkArgs ( argument1 , argument2 )
 	if expectedArgs.ARG2[argument2] then
 		arg2Valid = true
 	else
-		io.stderr:write("Argument type: '" .. argument2 .. "' is not valid or does not exist; run 'srunner --help'" , "\n")
+		local jsonLib = io.open("library.json","r+")
+		local jsonContent = jsonLib:read()
+		jsonLib:close()
+
+		local library = json.decode(jsonContent)
+
+		for i,v in pairs(library.Apps) do
+			if library.Apps[i]["ID"] == argument2 then
+				arg2Valid = true
+			end
+		end
+
+		if not arg2Valid then
+			io.stderr:write("Argument type: '" .. argument2 .. "' is not valid or does not exist; run 'srunner --help'" , "\n")
+		end
 	end
 
 	return arg1Valid, arg2Valid
-end
+end]]
 
-local function checkLibArg ( argument3 )
+--[[local function checkLibArg ( argument3 )
 	local IDValid = false
 	local appNameValid = false
 
@@ -87,23 +264,34 @@ local function checkLibArg ( argument3 )
 		if not IDValid then
 			io.stderr:write("App ID: '" .. argument3 .. "' does not exist or is not an installed app" , "\n")
 		end
+	else
+		IDValid = true
 	end
 
 	return IDValid
-end
+end]]
 
 steamLibHandler.UpdateLibraryJson()
 
-isArg1Valid, isArg2Valid = checkArgs( ARG1 , ARG2 )
+if validateArgs(ARG1,ARG2,ARG3) then
+	argFunctions[ARG1](ARG2,ARG3)
+end
+
+--[[isArg1Valid, isArg2Valid = checkArgs( ARG1 , ARG2 )
 
 isIDValid = checkLibArg( ARG3 )
 
 if isArg1Valid and isArg2Valid and isIDValid then
-	io.stderr:write('App loading...' , "\n")
+	--io.stderr:write('App loading...' , "\n")
 
-	os.execute("steam steam://rungameid/" .. ARG3 .. "&> /dev/null 2>&1")
+	--os.execute("steam steam://rungameid/" .. ARG3 .. "&> /dev/null 2>&1")
+	expectedArgs.ARG1[ARG1]( ARG2 , ARG3 )
+end]]
 
-end
+
+
+
+
 
 --[[term.clear()
 local defaultMode = term.savemode()
--- steamLibHandler.lua
+++ steamLibHandler.lua
@@ -16,6 +16,11 @@ local function scandir(directory)
     return t
 end
 
+function file_exists(name)
+   local f=io.open(name,"r")
+   if f~=nil then io.close(f) return true else return false end
+end
+
 function steamLibHandler.UpdateLibraryJson () 
 
 	local library = {
@@ -26,20 +31,26 @@ function steamLibHandler.UpdateLibraryJson ()
 	}
 
 	local homeEnv = os.getenv("HOME")
-	local libDirectory = homeEnv .. "/.local/share/Steam/steamapps/common"
+	local libDirectory = homeEnv .. "/.steam/steam/steamapps/common"
 
 	local libDirTable = scandir(libDirectory)
 
 	for i,v in pairs(libDirTable) do
 		if not(v == "." or v == "..") then
-			local idFileDir = libDirectory .. "/" .. v .. "/" .. "steam_appid.txt"
-			local appIDFile = io.open(idFileDir, "r")
-			local content = appIDFile:read()
-			io.close(appIDFile)
+			if file_exists(libDirectory .. "/" .. v .. "/" .. "steam_appid.txt") then
+				local idFileDir = libDirectory .. "/" .. v .. "/" .. "steam_appid.txt"
+				local appIDFile = io.open(idFileDir, "r")
+				local content = appIDFile:read()
+				io.close(appIDFile)
 
-			library.Apps[v] = {}
+				library.Apps[v] = {}
 
-			library.Apps[v]["ID"] = tostring(content)
+				library.Apps[v]["ID"] = tostring(content)
+			else
+				library.Apps[v] = {}
+
+				library.Apps[v]["ID"] = tostring("Unavailable")
+			end
 		end
 	end
 
